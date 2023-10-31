0512e01646895f2eb3d1c11915079e213dff1889
manolo777 <manolischristakis2002@gmail.com>
2023-10-31 19:37:40 +0200

    first commit

---
+++ gmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<gmp.h>
+
+int main(int argc,char *argv[]) {
+  
+	 mpz_t a,b,x;
+
+    /* initialize */
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(x);
+
+    /* set values */
+    mpz_set_ui(a,3000000000);    /* three billion */
+    mpz_set_ui(b,5000000000);    /* five billion */
+
+    /* add the values */
+    mpz_add(x,a,b);
+    /* output */
+    gmp_printf("%Zd + %Zd = %Zd\n",a,b,x);
+
+    return(0);
+
+  }
